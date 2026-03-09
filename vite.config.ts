d138f7927a8122079a6dbe86a5ffce74f0029530
zack427 <isaacmoraes1977@gmail.com>
2026-03-09 16:36:12 -0300

    Deploy rápido do site

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dia-das-mulheres-/", // ⚡ adicione essa linha
+  base: "/dia-das-mulheres-/", // ⚡ essencial para GitHub Pages
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
